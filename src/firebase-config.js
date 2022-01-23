ba03fd361ffd6cae14ff5aee63977e61faaa9ded
bakhtiarabid <abid.bu16@gmail.com>
2022-01-23 16:33:12 +0600

    Firebase added

---
+++ src/firebase-config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+   apiKey: "AIzaSyA9yb4wEcmxxwYX_ZA-rgVXE6xGqzLzums",
+   authDomain: "firebse-curd-2473c.firebaseapp.com",
+   projectId: "firebse-curd-2473c",
+   storageBucket: "firebse-curd-2473c.appspot.com",
+   messagingSenderId: "371063122945",
+   appId: "1:371063122945:web:17d37d94ae2c7a7243e400",
+   measurementId: "G-S8NPB5CL8R",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
