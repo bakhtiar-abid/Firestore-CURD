9cf9ea1db015458c8db9d47b63a79b7a038b6f52
bakhtiarabid <abid.bu16@gmail.com>
2022-01-23 16:54:30 +0600

    Firestore Added

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "@firebase/firestore";
 
 const firebaseConfig = {
    apiKey: "AIzaSyA9yb4wEcmxxwYX_ZA-rgVXE6xGqzLzums",
@@ -12,3 +13,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+ 
+ export const db = getFirestore(app);
